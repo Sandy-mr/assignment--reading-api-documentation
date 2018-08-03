17a796d548eec0d36a7c62f95aca6c90b71fdf41
Sandy-mr <sandy.mulato.r@gmail.com>
2018-08-02 23:10:09 -0500

    part 2 reading api docs

--- js/api-03-google-sunrise-darksky.js
+++ js/api-03-google-sunrise-darksky.js
@@ -21,25 +21,59 @@
 //
 
 const answerElement_apimashup_1 = document.getElementById('apimashup-1')
+var montreal = 'https://maps.googleapis.com/maps/api/geocode/json?address=Montreal';
+request.get(montreal).then(function(names){
+       elements = names.body.results;
+       console.log(elements[0].geometry.location)
+       var info = elements[0].geometry.location;
+       latGlobal = info.lat;
+       longGlobal = info.lng;
+       console.log(latGlobal);
+       console.log(longGlobal);
 
-
-
+       answerElement_apimashup_1.innerHTML = '<span id="lat">'+latGlobal+'</span><span>,</span><span id="long">'+longGlobal+'</span>' ;
+     })
 
 //========================================================================
 //  (2) What time does the sun set in Montreal (based on the Google geocode coordinates)?
 
 const answerElement_apimashup_2 = document.getElementById('apimashup-2')
-
-
-
-
-
+setTimeout(function(){
+  var lat = document.getElementById('lat').textContent;
+  var long = document.getElementById('long').textContent;
+  sunset = 'https://api.sunrise-sunset.org/json?lat='+lat+'&lng='+long+'';
+  request.get(sunset).then(function(names){
+    elements = names.body.results;
+    sunsetMontreal = elements.sunset
+    answerElement_apimashup_2.innerHTML = sunsetMontreal;
+  })
+}, 1000);
 
 //========================================================================
 //  (3) What is the weekly weather forecast in Montreal? (look for summary property in 'daily')
 
 const answerElement_apimashup_3 = document.getElementById('apimashup-3')
+//https://api.darksky.net/forecast/700bf35b241f390062f821a55cc27f78/45.5016889,-73.567256
 
+  setTimeout(function(){
+    var lat = document.getElementById('lat').textContent;
+    var long = document.getElementById('long').textContent;
+    var key = '700bf35b241f390062f821a55cc27f78';
+    var forecast = 'https://api.darksky.net/forecast/'+key+'/'+lat+','+long;
+    console.log(forecast)
+    console.log(key)
+    request.get(forecast).then(function(names){
+      elements = names.body;
+      weather = elements.daily.summary;
+      answerElement_apimashup_3.innerHTML =  weather;
+    })
+
+  }, 1000);
+
+
+
+
+//
 
 
 
--- js/api-04-tv-maze.js
+++ js/api-04-tv-maze.js
@@ -8,3 +8,38 @@ https://www.tvmaze.com/ap
 
 3. How many shows has actor Andrew Grimes (of the Walking Dead) appeared in?
 */
+const answerElement_tvmaze = document.getElementById('tvmaze-1')
+url = 'http://api.tvmaze.com/shows/618'
+
+request.get(url).then(function(names){
+  elements = names.body;
+  ratingShow = elements.rating.average
+  console.log(ratingShow)
+  answerElement_tvmaze.innerHTML += ratingShow;
+})
+
+//2. When was the premiere date for the 9th season of Friends?
+
+const answerElement_tvmaze_2 = document.getElementById('tvmaze-2')
+friends = 'http://api.tvmaze.com/shows/431/episodebynumber?season=9&number=1';
+request.get(friends).then(function(names){
+    elements = names.body;
+    console.log(elements);
+    premiere = elements.airdate;
+    console.log(premiere);
+    answerElement_tvmaze_2.innerHTML += premiere;
+  })
+
+
+
+//3. How many shows has actor Andrew Grimes (of the Walking Dead) appeared in?
+
+const answerElement_tvmaze_3 = document.querySelector('#tvmaze-3')
+
+walking = 'http://api.tvmaze.com/people/10257/castcredits';
+request.get(walking).then(function(names){
+    elements = names.body;
+    console.log(elements)
+    appears = elements.length;
+    answerElement_tvmaze_3.innerHTML += appears;
+  })
